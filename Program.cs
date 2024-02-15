47c428e17fcba150e8f34b55d996da7e01932600
abc <abc@gmail.com>
2024-02-15 14:52:43 +0530

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Item");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Jerry\t" + i);
+    Console.WriteLine("Tom\t" + i);
+}
+
+int[] num = { 10, 90, 40, 35 };
+int[] M1 = { 100, 101, 102, 103 };
+
+foreach (int item in num)
+{
+    Console.WriteLine(item);
+}
+foreach (int item in M1)
+{
+    Console.WriteLine(item);
+}
